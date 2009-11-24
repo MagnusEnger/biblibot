0137f4adb062a889d392b18e397b93aec72c0c61
Magnus Enger <magnus@enger.priv.no>
2009-11-24 23:37:30 +0100

    Append result to blip. Delete commented code.

--- biblibot.py
+++ biblibot.py
@@ -40,29 +40,14 @@ def OnBlipSubmitted(properties, context):
 
   # do the actual replacement
   for isbn in isbns:
-    # imdbID = url.group('movieID')
-    # gadgetUrl = 'http://imdbotty.appspot.com/gadget.xml?movieID=%s' % imdbID
-
-    # logging.debug('Inserting gadget with url %s' % gadgetUrl)
-    
-    # doc.DeleteRange(document.Range(url.start(), url.end()))
-    # gadget = document.Gadget(gadgetUrl)
-    # doc.InsertElement(isbn.start(), "test")
-    
-    # The URL must use the standard ports for HTTP (80) and HTTPS (443). 
-    # An app cannot connect to an arbitrary port of a remote host, nor can it use a non-standard port for a scheme.
-    # url = "http://dev.bibpode.no:9999/biblios?version=1.1&operation=searchRetrieve&startRecord=1&maximumRecords=10&query=dc.isbn=" + isbn.group('ISBN')
     
     url = "http://partner.bokkilden.no/SamboWeb/partner.do?rom=MP&format=XML&uttrekk=2&pid=0&ept=3&xslId=117&antall=3&frisok_omraade=3&frisok_sortering=0&frisok_tekst=" + isbn.group('ISBN')
-    # root_wavelet = context.GetRootWavelet()
-    # root_wavelet.CreateBlip().GetDocument().SetText("Url: %s" % url)
     result = urlfetch.fetch(url)
     if result.status_code == 200:
       dom = xml.dom.minidom.parseString(result.content)
       root_wavelet = context.GetRootWavelet()
-      root_wavelet.CreateBlip().GetDocument().SetText(isbn.group('ISBN') + " = " + getText(dom.getElementsByTagName("Tittel")[0].childNodes) + " av " + getText(dom.getElementsByTagName("Forfatter")[0].childNodes))
-      # root_wavelet.CreateBlip().GetDocument().SetText(result.content)
-      # dom.unlink()
+      doc.AppendText(" \n " + getText(dom.getElementsByTagName("Tittel")[0].childNodes) + " av " + getText(dom.getElementsByTagName("Forfatter")[0].childNodes))
+      dom.unlink()
 
 def Notify(context):
   root_wavelet = context.GetRootWavelet()
@@ -71,7 +56,7 @@ def Notify(context):
 if __name__ == '__main__':
   myRobot = robot.Robot('biblibot', 
       image_url='http://biblibot.appspot.com/assets/biblibot.png',
-      version='0.39',
+      version='0.61',
       profile_url='http://libriotech.no/')
   myRobot.RegisterHandler(events.WAVELET_SELF_ADDED, OnRobotAdded)
   myRobot.RegisterHandler(events.BLIP_SUBMITTED, OnBlipSubmitted)
