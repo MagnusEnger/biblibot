8693f1bf1edfdf32747c9ba727fd4e82b9d7f535
Magnus Enger <magnus@enger.priv.no>
2010-01-03 18:16:26 +0100

    Add template for /

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
-#
-# Copyright 2007 Google Inc.
+
+# Copyright 2007 Libriotech
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -15,19 +15,18 @@
 # limitations under the License.
 #
 
-
-
-
+import os
 import wsgiref.handlers
 
-
 from google.appengine.ext import webapp
-
+from google.appengine.ext.webapp import template
 
 class MainHandler(webapp.RequestHandler):
 
   def get(self):
-    self.response.out.write('Hello world!')
+    template_values = {}
+    path = os.path.join(os.path.dirname(__file__), 'templates/index.html')
+    self.response.out.write(template.render(path, template_values))
 
 
 def main():
